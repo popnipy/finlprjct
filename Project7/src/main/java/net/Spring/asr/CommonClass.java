8fefbbcf71df3499571127c564548c523693d3b4
Nifras Nipy <popnipy@gmail.com>
2019-08-06 17:17:09 +0530

    ch

--- Project7/src/main/java/net/Spring/asr/CommonClass.java
+++ Project7/src/main/java/net/Spring/asr/CommonClass.java
@@ -2,9 +2,9 @@ package net.Spring.asr;
 
 public class CommonClass {
 	public static void main(String args[]) {
-		String type = "Beach";
-		String sub1 = "Activity";
-		String sub2 = "Scenic";
+		String type = "Price";
+		String sub1 = "Distance";
+		String sub2 = "Availability";
 
 		Data_Common D = new Data_Common();
 		D.common(type, sub1, sub2);
